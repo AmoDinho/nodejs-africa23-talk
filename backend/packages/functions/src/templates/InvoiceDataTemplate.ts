4b78149bc146d463917faf54189b9366cfd2c9ed
AmoDinho <u14284783@tuks.co.za>
2023-02-15 22:30:36 +0200

    created template

--- backend/packages/functions/src/templates/InvoiceDataTemplate.ts
+++ backend/packages/functions/src/templates/InvoiceDataTemplate.ts
@@ -1,1 +1,18 @@
-export const InvoiceData = ({}) => {};
+const head = `
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
+    <title>Load confirmation</title>
+    </head>
+    `;
+export const InvoiceData = ({}) => `
+<html>
+${head}
+<body>
+<div>c
+<h1>hello nodejs23</h1>
+</div>
+</body>
+</html>
+`;
