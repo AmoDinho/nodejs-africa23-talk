fe9e6d101123011448337dd72c694a2e74535418
AmoDinho <u14284783@tuks.co.za>
2023-02-21 19:00:49 +0200

    Added Tailwind

--- backend/packages/functions/src/templates/InvoiceDataTemplate.ts
+++ backend/packages/functions/src/templates/InvoiceDataTemplate.ts
@@ -4,6 +4,18 @@ const head = `
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
     <title>Load confirmation</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#dc2626',
+          }
+        }
+      }
+    }
+  </script>
     </head>
     `;
 export const InvoiceDataTemplate = () => `
@@ -11,7 +23,7 @@ export const InvoiceDataTemplate = () => `
 ${head}
 <body>
 <div>c
-<h1>hello nodejs23</h1>
+<h1 class="text-clifford">hello nodejs23</h1>
 </div>
 </body>
 </html>
