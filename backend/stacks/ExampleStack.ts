2a0010d1975bb94c47b61d03982a69d3604a6ea4
AmoDinho <u14284783@tuks.co.za>
2023-02-23 21:25:56 +0200

    created bucket

--- backend/stacks/ExampleStack.ts
+++ backend/stacks/ExampleStack.ts
@@ -1,18 +1,20 @@
-import { GraphQLApi, StackContext, use } from '@serverless-stack/resources';
+import { GraphQLApi, StackContext, Bucket } from '@serverless-stack/resources';
 import { AddLayerVersionPermissionCommand } from '@aws-sdk/client-lambda';
 import { AddLayerVersionPermissionCommandInput } from '@types/aws-lambda';
-import ExampleStackResources from './S3Stack';
+
 export default function ExampleStack({ stack }: StackContext) {
   // Create the GraphQL AP
+  // const { BackendStack } = use(ExampleStackResources);
+  const InvoiceBucket = new Bucket(stack, 'invoice-bucket');
   const layerArn: string = `arn:aws:lambda:us-east-1:764866452798:layer:chrome-aws-lambda:31`;
 
   const layerConfig: AddLayerVersionPermissionCommandInput = {
     Action: 'lambda:AddLayerVersion',
     LayerName: layerArn,
   };
-  const { BackendStack } = use(ExampleStackResources);
 
   const layer = new AddLayerVersionPermissionCommand(layerConfig);
+
   const api = new GraphQLApi(stack, 'ApolloApi', {
     server: 'packages/functions/src/lambda.handler',
     defaults: {
@@ -20,9 +22,9 @@ export default function ExampleStack({ stack }: StackContext) {
         timeout: 20,
         environment: {
           AWS_ACCOUNT_NUMBER: process.env.AWS_ACCOUNT_NUMBER,
-          InvoiceBucketResource: BackendStack.S3Resources.bucketName,
+          InvoiceBucketResource: InvoiceBucket.bucketName,
         },
-        permissions: [BackendStack.S3Resources.bucketName],
+        permissions: [InvoiceBucket.bucketName],
         bundle: {
           // format: 'esm',
           externalModules: ['@sparticuz/chrome-aws-lambda', 'mitt'],
