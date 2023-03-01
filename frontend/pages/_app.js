ffa9a731f0406e1f62c332ed9f92bacba1bbc523
AmoDinho <u14284783@tuks.co.za>
2023-03-01 22:12:54 +0200

    installed chakra

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../lib/apolloClient';
-
+import { ChakraProvider } from '@chakra-ui/react';
 export default function App({ Component, pageProps }) {
   // const apolloClient = useApollo(pageProps);
 
