ab3dc1d49b0810f782a8a84ee9356312f75fa28b
AmoDinho <u14284783@tuks.co.za>
2023-03-01 21:55:18 +0200

    runnig the app

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,34 +1,33 @@
-import App from '../components/App'
-import InfoBox from '../components/InfoBox'
-import Header from '../components/Header'
-import Submit from '../components/Submit'
+import App from '../components/App';
+import InfoBox from '../components/InfoBox';
+import Header from '../components/Header';
+import Submit from '../components/Submit';
 import PostList, {
   ALL_POSTS_QUERY,
   allPostsQueryVars,
-} from '../components/PostList'
-import { initializeApollo, addApolloState } from '../lib/apolloClient'
+} from '../components/PostList';
+import { initializeApollo, addApolloState } from '../lib/apolloClient';
 
 const IndexPage = () => (
   <App>
     <Header />
     <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
-    <Submit />
-    <PostList />
+    {/* <Submit />
+    <PostList /> */}
   </App>
-)
+);
 
 export async function getStaticProps() {
-  const apolloClient = initializeApollo()
-
-  await apolloClient.query({
-    query: ALL_POSTS_QUERY,
-    variables: allPostsQueryVars,
-  })
-
-  return addApolloState(apolloClient, {
-    props: {},
-    revalidate: 1,
-  })
+  return { props: {} };
+  // const apolloClient = initializeApollo()
+  // await apolloClient.query({
+  //   query: ALL_POSTS_QUERY,
+  //   variables: allPostsQueryVars,
+  // })
+  // return addApolloState(apolloClient, {
+  //   props: {},
+  //   revalidate: 1,
+  // })
 }
 
-export default IndexPage
+export default IndexPage;
