08cacdadaf183e6ba63ea03783a2c7c2b2ce6568
AmoDinho <u14284783@tuks.co.za>
2023-03-02 22:08:35 +0200

    created detail item

--- frontend/components/TripList.js
+++ frontend/components/TripList.js
@@ -1,6 +1,23 @@
+const TripListItem = ({ tripDetail }) => {
+  if (!tripDetail) return <p>Unable to render trip detail</p>;
+  return (
+    <div className="flex flex-row">
+      <p>{tripDetail.guide}</p>
+      <p>{tripDetail.price}</p>
+      <p>{tripDetail.tripName}</p>
+    </div>
+  );
+};
+
 const TripList = ({ tripPayload }) => {
   if (!tripPayload) return <p>Unabale to render payload</p>;
-  return <div></div>;
+  return (
+    <div className="flex flex-col">
+      {tripPayload.map((trip) => (
+        <TripList tripDetail={trip} />
+      ))}
+    </div>
+  );
 };
 
 export default TripList;
