1f5e585cabc72acbafdf2fb95fc62d40583ee1b3
AmoDinho <u14284783@tuks.co.za>
2023-03-02 22:22:10 +0200

    components

--- frontend/components/TripList.js
+++ frontend/components/TripList.js
@@ -1,8 +1,25 @@
+const ErrorComponent = (payload) => {
+  if (!payload) return <p className="text-red-300">Unable to render payload</p>;
+};
+
+const TripPropertyColum = ({ columnData }) => {
+  return (
+    <>
+      <ErrorComponent payload={columnData} />
+      <div className="flex flex-col">
+        <p>{columnData.title}</p>
+        <p>{columnData.value}</p>
+      </div>
+    </>
+  );
+};
+
 const TripListItem = ({ tripDetail }) => {
   if (!tripDetail) return <p>Unable to render trip detail</p>;
   return (
     <div className="flex flex-row">
-      <p>{tripDetail.guide.name}</p>
+      <TripPropertyColum title="Guide Name" value={tripDetail.guide.name} />
+
       <p>{tripDetail.price}</p>
       <p>{tripDetail.listingName}</p>
     </div>
